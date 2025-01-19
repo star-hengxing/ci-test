0866f91c1f8d4684e506efc7a932acc6a4bdc7ec
star9029 <hengxings783@gmail.com>
2025-01-19 15:09:30 +0800

    test xmake action

--- xmake.lua
+++ xmake.lua
@@ -1,42 +1,5 @@
-set_policy("package.install_locally", true)
+add_requires("tbox")
 
-add_requires("test")
-
-package("test")
-    set_sourcedir("src")
-
-    on_install(function (package)
-        io.writefile("xmake.lua", [[
-            add_rules("mode.debug", "mode.release")
-            target("test")
-                set_kind("static")
-                add_files("*.cpp")
-        ]])
-        import("package.tools.xmake").install(package)
-
-        local abi
-        local format
-        if package:is_plat("windows") then
-            abi = "msvc"
-            format = ".7z"
-        elseif package:is_plat("linux") then
-            abi = "gnu"
-            format = ".tar.xz"
-        end
-        -- arch-plat-abi-mode
-        local archive_name = table.concat({
-            package:arch(),
-            package:plat(),
-            abi,
-            package:is_debug() and "debug" or "release",
-        }, "-")
-
-        local archive_file = path.join(os.scriptdir(), "build/package", archive_name .. format)
-
-        local opt = {}
-        opt.recurse = true
-        opt.compress = "best"
-        import("utils.archive").archive(archive_file, package:installdir(), opt)
-
-        print(hash.sha256(archive_file))
-    end)
+target("test")
+    set_kind("binary")
+    add_files("src/*.cpp")
