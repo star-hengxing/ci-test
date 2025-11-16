62053045384746f4ca236454a923140647337873
star9029 <hengxings783@gmail.com>
2025-11-16 21:10:48 +0800

    fix lto

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,14 @@ set_policy("build.optimization.lto", true)
 target("right")
     set_kind("binary")
     add_files("src/*.cpp")
-    set_toolset("dsymutil", "dsymutil")
+    if is_plat("macosx") then
+        set_toolset("dsymutil", "dsymutil")
+    end
+
+    on_load(function (target)
+        target:add("ldflags", format("-object_path_lto,%s_lto.o", target:name()))
+        target:add("ldflags", "-cache_path_lto," .. target:targetdir())
+    end)
 
 target("error")
     set_kind("binary")
